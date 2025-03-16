692e3c578305d6dca6b09729c6ac12269d999009
JackGilinger <therzzanfcsty@gmail.com>
2025-03-16 16:55:22 +0300

    Full implementation of Diary of Emotions app screens and UI flows in AppWidget.dart

--- widgets/app_widget.dart
+++ widgets/app_widget.dart
@@ -11,8 +11,7 @@ class AppWidget extends StatefulWidget {
   final double width;
   final double height;
 
-  const AppWidget({required this.width, required this.height, Key? key})
-      : super(key: key);
+  const AppWidget({required this.width, required this.height, Key? key}) : super(key: key);
 
   @override
   _AppWidgetState createState() => _AppWidgetState();
@@ -26,6 +25,10 @@ class _AppWidgetState extends State<AppWidget> {
     switch (_currentScreen) {
       case 0:
         return _buildSplashScreen();
+      case 1:
+        return _buildOnboarding();
+      case 2:
+        return _buildDashboard();
       default:
         return _buildSplashScreen();
     }
@@ -33,9 +36,8 @@ class _AppWidgetState extends State<AppWidget> {
 
   Widget _buildSplashScreen() {
     Future.delayed(const Duration(seconds: 2), () {
-      setState(() {_currentScreen = 1;});
+      setState(() => _currentScreen = 1);
     });
-
     return Container(
       width: widget.width,
       height: widget.height,
@@ -43,8 +45,8 @@ class _AppWidgetState extends State<AppWidget> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Icon(Icons.book, size: 100.0, color: Color(0xFF6200EE)),
-          const SizedBox(height: 20),
+          Icon(Icons.book, size: 100, color: Color(0xFF6200EE)),
+          SizedBox(height: 20),
           Text(
             'Welcome to Diary of Emotions',
             style: TextStyle(
@@ -55,6 +57,76 @@ class _AppWidgetState extends State<AppWidget> {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildOnboarding() {
+    return Container(
+      width: widget.width,
+      height: widget.height,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Track and analyze your emotions every day!',
+            style: TextStyle(fontSize: 20, color: Colors.black),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              setState(() => _currentScreen = 2);
+            },
+            child: Text('Get Started'),
+            style: ElevatedButton.styleFrom(primary: Color(0xFF6200EE)),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDashboard() {
+    return Container(
+      width: widget.width,
+      height: widget.height,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Select your current emotion',
+            style: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              EmotionButton('😊', 'Happy'),
+              EmotionButton('😔', 'Sad'),
+              EmotionButton('😡', 'Angry'),
+              EmotionButton('😨', 'Anxious'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EmotionButton extends StatelessWidget {
+  final String emotionIcon;
+  final String emotionLabel;
+
+  EmotionButton(this.emotionIcon, this.emotionLabel);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(emotionIcon, style: TextStyle(fontSize: 40)),
+        SizedBox(height: 8),
+        Text(emotionLabel, style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
